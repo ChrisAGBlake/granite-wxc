e2ffe71b8bdac20e93d4022ec5fc30a030185352
Chris Blake <chris.a.g.blake@gmail.com>
2024-10-29 10:18:14 +1300

    added in gradient accumulation

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from omegaconf import OmegaConf
 from granitewxc.datasets.ecmwf import ECMWFDownscaleDataset
 from granitewxc.utils.downscaling_model import get_finetune_model
 import wandb
+import json
 import logging
 
 log = logging.getLogger(__name__)
@@ -33,9 +34,20 @@ else:
 # load the config
 config = OmegaConf.load('ecmwf_config.yaml')
 
+# setup the scalers
+with open('scalers.json', 'r') as f:
+    scalers = json.load(f)
+mu = []
+sigma = []
+for var in config.data.output_vars:
+    mu.append(scalers[var]['mu'])
+    sigma.append(scalers[var]['sigma'])
+mu = torch.tensor(mu).reshape(1, -1, 1, 1).to(device)
+sigma = torch.tensor(sigma).reshape(1, -1, 1, 1).to(device)
+
 # initialize wandb
-# wandb.login()
-# wandb.init(entity='chris_blake', project='pw-downscale', config=config)
+wandb.login()
+wandb.init(entity='chris_blake', project='pw-downscale', config=config)
 
 # setup the datasets and dataloaders
 files = list(glob(f'{config.data.parsed_data_dir}/*'))
@@ -51,40 +63,61 @@ val_dataloader = DataLoader(val_dataset, batch_size=config.train.batch_size, shu
 # setup the model
 model = get_finetune_model(config, logger=None)
 model.to(device)
-
+    
 # setup the loss function and optimizer
 loss = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=config.train.lr)
 print('train dataset size:', len(train_dataset))
+print('val dataset size:', len(val_dataset))
 
-for i in range(config.train.num_epochs):
+for n in range(config.train.num_epochs):
 
     # train
     tl = 0
-    i = 0
-    for batch in train_dataloader:
+    for i, batch in enumerate(train_dataloader):
+
+        # get the predictions
         batch = {k: v.to(device) for k, v in batch.items()}
-        optimizer.zero_grad()
         out = model(batch)
-        l = loss(out, batch['y'])
+
+        # scale the predictions and targets
+        out = (out - mu) / sigma
+        y = (batch['y'] - mu) / sigma
+
+        # get the loss
+        l = loss(out, y)
         l.backward()
-        optimizer.step()
+
+        # update the weights
+        if (i + 1) % config.train.grad_accum_steps == 0 or i + 1 == len(train_dataloader):
+            optimizer.step()
+            optimizer.zero_grad()
+
+        # logging
         tl += l.item()
         print(i, l.item(), end='\r')
-        i += 1
-    log.info(f'epoch: {i}, train loss: {tl}')
+        if (i + 1) % 100 == 0:
+            wandb.log({'train_loss': tl / 100})
+            tl = 0
     
     # evaluate
     vl = 0
     model.eval()
     with torch.no_grad():
         for batch in val_dataloader:
+
+            # get the predictions
             batch = {k: v.to(device) for k, v in batch.items()}
             out = model(batch)
-            l = loss(out, batch['y'])
-            vl += l.item()
-    log.info(f'epoch: {i}, valloss: {vl}')
-    model.train()
 
-    # log to wandb
-    # wandb.log({'train_loss': tl, 'val_loss': vl})
+            # scale the predictions and targets
+            out = (out - mu) / sigma
+            y = (batch['y'] - mu) / sigma
+
+            # get the loss
+            l = loss(out, y)
+            vl += l.item()
+    vl /= len(val_dataloader)
+    wandb.log({'val_loss': vl})
+    log.info(f'epoch: {n}, val loss: {vl}')
+    model.train()
