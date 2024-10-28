d2803880dc543d0cc16681cc205e9a8dc72ed6aa
Chris Blake <chris.a.g.blake@gmail.com>
2024-10-29 12:15:02 +1300

    updated eval using xarray regid to use grib files and whole globe

--- eval.py
+++ eval.py
@@ -3,6 +3,7 @@ from xarray_regrid import Grid, create_regridding_dataset
 import numpy as np
 from omegaconf import OmegaConf
 import json
+from glob import glob
 import logging
 
 log = logging.getLogger(__name__)
@@ -16,88 +17,77 @@ config = OmegaConf.load('ecmwf_config.yaml')
 # setup the scalers
 with open('scalers.json', 'r') as f:
     scalers = json.load(f)
-mu = []
-sigma = []
-for var in config.data.output_vars:
-    mu.append(scalers[var]['mu'])
-    sigma.append(scalers[var]['sigma'])
-mu = np.array(mu).reshape(-1, 1, 1)
-sigma = np.array(sigma).reshape(-1, 1, 1)
-
-# get the indices of the output variables within the input tensor
-idxs = []
-offset = len(config.data.input_vertical_vars) * len(config.data.input_levels)
-for var in config.data.output_vars:
-    for i in range(len(config.data.input_surface_vars)):
-        if config.data.input_surface_vars[i] == var:
-            idxs.append(i + offset)
-            break
-
-# load the list of validation files to evaluate
-with open('data/val_files.json', 'r') as f:
-    val_files = json.load(f)
 
 # step through the files and evaluate
-vl = 0
-for i, file in enumerate(val_files):
-    
+vars = config.data.output_vars
+files = [file for file in glob(f'{config.data.data_dir}/*') if '.idx' not in file]
+for file in files:
 
-    # load the sample
-    sample = np.load(file)
+    # load the grib file
+    ds = xr.open_dataset(file, engine='cfgrib')
 
-    # scale the input and output
-    x = (sample['x'][idxs, :, :] - mu) / sigma
-    y = (sample['y'] - mu) / sigma
-    
-    # get the lats and lons for the sample
-    filename = file.split('/')[-1]
-    chunks = filename.split('_')
-    lat_s = int(chunks[1])
-    lon_s = int(chunks[2])
-    sz = chunks[3]
-    sz = int(sz.split('.')[0])
-    lat_e = lat_s + sz
-    lon_e = lon_s + sz
-    x_lats = np.arange(lat_s, lat_e, 0.2, dtype=np.float32)
-    x_lons = np.arange(lon_s, lon_e, 0.2, dtype=np.float32)
-    y_lats = np.arange(lat_s, lat_e, 0.1, dtype=np.float32)
-    y_lons = np.arange(lon_s, lon_e, 0.1, dtype=np.float32)
+    # select the variables of interest
+    ds = ds[vars]
 
-    # generate an xarray dataset from x
-    ds = xr.Dataset(
-        {
-            't2m': (['lat', 'lon'], x[0, :, :]),
-            'u10': (['lat', 'lon'], x[1, :, :]),
-            'v10': (['lat', 'lon'], x[2, :, :]),
-            'msl': (['lat', 'lon'], x[3, :, :]),
-        },
-        coords={
-            'lat': x_lats,
-            'lon': x_lons,
-        }
-    )
+    # select the latitudes and longitudes of interest
+    x_lats = np.arange(-80, 80.05, 0.2, dtype=np.float32)
+    x_lons = np.arange(0, 360, 0.2, dtype=np.float32)
+    y_lats = np.arange(-80, 80.05, 0.1, dtype=np.float32)
+    y_lons = np.arange(0, 360, 0.1, dtype=np.float32)
+    y_lats_idxs = []
+    for lat in y_lats:
+        y_lats_idxs.append(np.argmin(np.abs(ds['latitude'].values - lat)))
+    y_lons_idxs = []
+    for lon in y_lons:
+        y_lons_idxs.append(np.argmin(np.abs(ds['longitude'].values - lon)))
+    ds = ds.isel(latitude=y_lats_idxs, longitude=y_lons_idxs)
+
+    # scale
+    for var in vars:
+        ds[var].values = (ds[var].values - scalers[var]['mu']) / scalers[var]['sigma']
+
+    # generate target array
+    target = np.zeros((len(vars), len(y_lats), len(y_lons)), dtype=np.float32)
+    for i, var in enumerate(vars):
+        target[i, :, :] = ds[var].values.copy()
+
+    # generate the low resolution input
+    x_lats_idxs = []
+    for lat in x_lats:
+        x_lats_idxs.append(np.argmin(np.abs(ds['latitude'].values - lat)))
+    x_lons_idxs = []
+    for lon in x_lons:
+        x_lons_idxs.append(np.argmin(np.abs(ds['longitude'].values - lon)))
+    inp_ds = ds.isel(latitude=x_lats_idxs, longitude=x_lons_idxs)
 
     # regrid the dataset to the target grid
-    d = y_lats[1] - y_lats[0]
     target_grid = Grid(
-        north=y_lats.max(),
-        east=y_lons.max(),
-        south=x_lats.min(),
-        west=y_lons.min(),
-        resolution_lat=d,
-        resolution_lon=d,
+        north=80.05,
+        east=360,
+        south=-80,
+        west=0,
+        resolution_lat=0.1,
+        resolution_lon=0.1,
     )
-    target_dataset = create_regridding_dataset(target_grid, lat_name='lat', lon_name='lon')
-    new_ds = ds.regrid.linear(target_dataset)
+    target_dataset = create_regridding_dataset(target_grid, lat_name='latitude', lon_name='longitude')
+    new_ds = inp_ds.regrid.linear(target_dataset)
+
+    # check the grids match in size
+    if new_ds['latitude'].values.shape[0] != y_lats.shape[0]:
+        print('latitudes do not match')
+        print(new_ds['latitude'].values.shape[0], y_lats.shape[0])
+        continue
+    if new_ds['longitude'].values.shape[0] != y_lons.shape[0]:
+        print('longitudes do not match')
+        print(new_ds['longitude'].values.shape[0], y_lons.shape[0])
+        continue
+
+    # generate the pred array
+    pred = np.zeros((len(vars), len(y_lats), len(y_lons)), dtype=np.float32)
+    for i, var in enumerate(vars):
+        pred[i, :, :] = new_ds[var].values.copy()
 
     # calculate the loss
-    pred = np.zeros((4, 199, 199), dtype=np.float32)
-    pred[0, :, :] = new_ds['t2m'].values[:199, :199]
-    pred[1, :, :] = new_ds['u10'].values[:199, :199]
-    pred[2, :, :] = new_ds['v10'].values[:199, :199]
-    pred[3, :, :] = new_ds['msl'].values[:199, :199]
-    y = y[:, :199, :199]
-    l = np.mean((pred - y)**2)
-    vl += l
-    print(f'{i}/{len(val_files)}, loss: {l:.3f}', end='\r')
+    l = np.mean((pred - target)**2)
+    print(f'{file}, loss: {l:.3f}')
 
